054751afe7c8da1f0860cbaa3822ec0ce2e0a003
unknown <mohsinsait23@gmail.com>
2020-11-03 19:16:52 +0530

    added Link tag from react-router-dom and linked all the pages

--- myportfoliosite/src/App.js
+++ myportfoliosite/src/App.js
@@ -2,31 +2,34 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{height: '700px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
-        </Drawer>
-        <Main />
-        <Content />
-    </Layout>
-</div>
+    <div className="demo-big-content">
+        <Layout>
+            <Header title="Title" scroll>
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main />
+            </Content>
+        </Layout>
+    </div>
   );
 }
 
--- myportfoliosite/src/components/main.js
+++ myportfoliosite/src/components/main.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
+import Resume from './resume';
+import About from './aboutme';
+import Projects from './projects';
+import Contact from './contact';
 
 const Main = () => (
 	<Switch>
 		<Route exact path="/" component={LandingPage} />
+		<Route path="/resume" component={Resume} />
+		<Route path="/aboutme" component={About} />
+		<Route path="/projects" component={Projects} />
+		<Route path="/contact" component={Contact} />
 	</Switch>
 )
 
