e48960ed3b2ac72c592a386d9f2b5f562da7aa0e
unknown <mohsinsait23@gmail.com>
2020-11-04 19:51:27 +0530

    added two more cards and diplayed it as flexbox

--- myportfoliosite/src/components/projects.js
+++ myportfoliosite/src/components/projects.js
@@ -10,19 +10,52 @@ class Project extends Component {
 	toggleCategories() {
 		if(this.state.activeTab === 0){
 			return(
-				<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-					<CardTitle style={{color: 'black', height: '176px', background: 'url(https://d1afx9quaogywf.cloudfront.net/sites/default/files/Logos/Instagram400x230.png) center / cover'}}>Instagram-clone</CardTitle>
-					<CardText>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-					</CardText>
-					<CardActions border>
-						<Button colored>Github</Button>
-						<Button colored>Live Demo</Button>
-					</CardActions>
-					<CardMenu style={{color: '#fff'}}>
-						<IconButton name="share" />
-					</CardMenu>
-				</Card>
+				<div className="project-grid">
+					{/* Project 1 */}
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.etimg.com/thumb/msid-78516741,width-1200,height-900/magazines/panache/instagram-turns-10-launches-new-well-being-features.jpg) center / cover'}}>Instagram-clone</CardTitle>
+						<CardText>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</CardText>
+						<CardActions border>
+							<Button colored>Github</Button>
+							<Button colored>Live Demo</Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+
+					{/* Project 1 */}
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.etimg.com/thumb/msid-78516741,width-1200,height-900/magazines/panache/instagram-turns-10-launches-new-well-being-features.jpg) center / cover'}}>Instagram-clone</CardTitle>
+						<CardText>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</CardText>
+						<CardActions border>
+							<Button colored>Github</Button>
+							<Button colored>Live Demo</Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+
+					{/* Project 1 */}
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.etimg.com/thumb/msid-78516741,width-1200,height-900/magazines/panache/instagram-turns-10-launches-new-well-being-features.jpg) center / cover'}}>Instagram-clone</CardTitle>
+						<CardText>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</CardText>
+						<CardActions border>
+							<Button colored>Github</Button>
+							<Button colored>Live Demo</Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+				</div>
 			)
 		} else if(this.state.activeTab === 1) {
 			return(
@@ -40,13 +73,11 @@ class Project extends Component {
 					<Tab>Django</Tab>
 				</Tabs>
 
-				<section className="project-grid">
-					<Grid className="project-grid">
-						<Cell col={12}>
-							<div className="content">{this.toggleCategories()}</div>
-						</Cell>
-					</Grid>
-				</section>
+				<Grid>
+					<Cell col={12}>
+						<div className="content">{this.toggleCategories()}</div>
+					</Cell>
+				</Grid>
 			</div>
 		)
 	}
