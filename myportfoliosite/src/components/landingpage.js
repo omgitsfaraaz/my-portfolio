1e1de42189409a92e4ce8a6e82d4e579e2996f84
unknown <mohsinsait23@gmail.com>
2020-11-04 12:21:05 +0530

    added social media links

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -18,6 +18,27 @@ class Landing extends Component {
 							<h1>Keen Interest in Frontend Developement</h1>
 							<hr/>
 							<p>HTML/CSS | Bootstrap | JavaScript | React JS | NodeJS | MongoDB</p>
+							<div className="social-links">
+								{/* Linkedin */}
+								<a href="https://linkedin.com/in/mohammed-mohsin-sait-935a8b184" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-linkedin-square" aria-hidden="true"></i>
+								</a>
+
+								{/* Github */}
+								<a href="https://www.google.com/" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-github-square" aria-hidden="true"></i>
+								</a>
+
+								{/* Instagram */}
+								<a href="https://www.google.com/" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-instagram" aria-hidden="true"></i>
+								</a>
+
+								{/* Skype */}
+								<a href="https://www.google.com/" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-skype" aria-hidden="true"></i>
+								</a>
+							</div>
 						</div>
 
 					</Cell>
