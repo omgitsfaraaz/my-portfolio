090cf1ec80ad3020691c99f46bb0ba96ca860f84
unknown <mohsinsait23@gmail.com>
2020-11-04 10:58:00 +0530

    added avatar

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import picture from './circle-cropped.png';
 
 class Landing extends Component {
 	render() {
 		return(
 			<div style={{width:'100%', margin: 'auto'}}>
 				<Grid className="landing-grid">
-					<Cell col={12}></Cell>
+					<Cell col={12}>
+						<img 
+							src={picture} 
+							alt="avatar"
+							className="avatar-img"
+						/>
+					</Cell>
 				</Grid>
 			</div>
 		)
