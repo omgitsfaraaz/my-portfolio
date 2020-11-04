b3e042244040fe4c6fe637d4ef803623ad11de6a
unknown <mohsinsait23@gmail.com>
2020-11-04 13:57:55 +0530

    added separate tabs for projects

--- myportfoliosite/src/components/projects.js
+++ myportfoliosite/src/components/projects.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Project extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { activeTab: 0 }
+	}
+
+
 	render() {
 		return(
-			<div>
-				<h1>Project page.</h1>
+			<div className="category-tabs">
+				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+					<Tab>React JS</Tab>
+					<Tab>Django</Tab>
+				</Tabs>
 			</div>
 		)
 	}
