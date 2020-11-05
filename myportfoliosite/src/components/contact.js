6bbd2900d6559769bc00c3d732729fc8428efffb
unknown <mohsinsait23@gmail.com>
2020-11-05 13:24:21 +0530

    added name and image in contacts page

--- myportfoliosite/src/components/contact.js
+++ myportfoliosite/src/components/contact.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import picture from './circle-cropped.png';
 
 class Contact extends Component {
 	render() {
 		return(
 			<div className="contact-body">
 				<Grid className="contact-grid">
-					<Cell col={6}>half page</Cell>
+					<Cell col={6}>
+						<h2>Mohammed Mohsin Sait</h2>
+						<img 
+							src={picture} 
+							alt="avatar"
+							style={{height: '250px'}}
+						/>
+					</Cell>
+
+
 					<Cell col={6}>half page</Cell>
 				</Grid>
 			</div>
