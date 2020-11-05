74530b3b5cec04c15480cab4925a9e1f0d200426
unknown <mohsinsait23@gmail.com>
2020-11-05 18:46:07 +0530

    added styling for different media

--- myportfoliosite/src/components/contact.js
+++ myportfoliosite/src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 import picture from './circle-cropped.png';
 
 class Contact extends Component {
@@ -14,10 +14,29 @@ class Contact extends Component {
 							alt="avatar"
 							style={{height: '250px'}}
 						/>
+						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Good at Django, Python, Java. Love to create real life projects. I'm basically concentrating more on Django and React JS projects.</p>
 					</Cell>
 
 
-					<Cell col={6}>half page</Cell>
+					<Cell col={6}>
+						<h2>Contact Me</h2>
+						<hr />
+
+						<List>
+						  <ListItem>
+						    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+						    	<i className="fa fa-phone-square"></i>+91 88801 04380
+						    </ListItemContent>
+						  </ListItem>
+						  <ListItem>
+						    <ListItemContent icon="person">Aaron Paul</ListItemContent>
+						  </ListItem>
+						  <ListItem>
+						    <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+						  </ListItem>
+						</List>
+
+					</Cell>
 				</Grid>
 			</div>
 		)
