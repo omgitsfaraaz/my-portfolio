e349e80bee9fa04db7f377de37a116120e369533
unknown <mohsinsait23@gmail.com>
2020-11-07 12:10:28 +0530

    added image and added styling to it

--- myportfoliosite/src/components/resume.js
+++ myportfoliosite/src/components/resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import picture from './circle-cropped.png';
 
 
 class Resume extends Component {
@@ -7,8 +8,19 @@ class Resume extends Component {
 		return(
 			<div>
 				<Grid>
-					<Cell col={4}>Left side</Cell>
-					<Cell col={8}>Right side</Cell>
+					<Cell className="resume-left-col" col={4}>
+						<div style={{textAlign: 'center'}}>
+							<img 
+								src={picture} 
+								alt="avatar"
+								style={{height: '200px'}}
+							/>
+						</div>
+					</Cell>
+
+
+
+					<Cell className="resume-right-col" col={8}>Right side</Cell>
 				</Grid>
 			</div>
 		)
